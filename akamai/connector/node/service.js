da0f0e91cb75ba94e3b2a0bb176f48d514c9457f
lcassiano <leandro@wetware.com.br>
2022-07-11 16:52:14 -0300

    change kafka config

--- akamai/connector/node/service.js
+++ akamai/connector/node/service.js
@@ -1,17 +1,23 @@
 const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
-  clientId: 'akamai-dsadsdsa',
-  brokers: ['localhost:9092']
+  clientId: 'akamai-siem',
+  brokers: ['192.168.1.137:9092']
 })
 
-const producer = kafka.producer()
+const producer = kafka.producer() 
 
-producer.connect()
-producer.send({
-topic: 'aka-siem-sjakdjks',
-messages: [
-    { value: 'Hello KafkaJS user!' },
-],
-})
+var sendMessage = async () => {
+  await producer.connect()
+  await producer.send({
+    topic: 'akamai-siem',
+    messages: [
+      { key: 'key1', value: 'hello world' },
+      { key: 'key2', value: 'hey hey!' }
+    ],
+  })
+  await producer.disconnect()
+}
+
+sendMessage();
 
