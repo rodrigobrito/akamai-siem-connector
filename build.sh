a2cbaf616de7ff1311d14623c04a46d33bf11ea4
fvilarinho <fvilarinho@gmail.com>
2022-07-26 20:13:06 -0300

    Change from ELK to opensearch.

--- build.sh
+++ build.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
+echo Building base-consumer
 cd base-consumer
 
 ./build.sh
 
+echo
+echo Building consumer
 cd ../consumer
 
 ./build.sh
 
+echo
+echo Building base-processor
 cd ../base-processor
 
 ./build.sh
 
+echo
+echo Building processor-kafka
 cd ../processor-kafka
 
 ./build.sh
 
 cd ..
 
+echo
+echo Building docker images
 docker-compose --profile build build
