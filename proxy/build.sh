bf9b9d8dac1f63d4b038e49b268db2a568468aec
Leandro Cassiano <leandro@wetware.com.br>
2022-08-09 13:50:32 +0000

    Change logstash image and config

--- proxy/build.sh
+++ proxy/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+mkdir ssl
 openssl req -x509 -nodes -days 365 -subj "/C=BR/ST=SP/O=lcassiano.me, Inc./CN=lcassiano.me" -addext "subjectAltName=DNS:siem.lcassiano.me" -newkey rsa:2048 -keyout ssl/nginx-selfsigned.key -out ssl/nginx-selfsigned.crt
