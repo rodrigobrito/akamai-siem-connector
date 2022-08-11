81f6823eb3f80459726fb86e1286c9edac742199
Felipe Vilarinho <fvilarinho@gmail.com>
2022-08-11 11:25:52 -0300

    Code improvements in logging and data storage.

--- kafka/zookeeper/bin/startup.sh
+++ kafka/zookeeper/bin/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Setup the startup.
 $BIN_DIR/setup.sh
 
-/opt/kafka/bin/zookeeper-server-start.sh $ETC_DIR/zookeeper.properties
+# Start zookeeper specifying the configuration file.
+/opt/kafka/bin/zookeeper-server-start.sh $ETC_DIR/zookeeper.properties > $LOGS_DIR/zookeeper.log &
+
+tail -f $LOGS_DIR/zookeeper.log
