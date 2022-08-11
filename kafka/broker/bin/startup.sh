129ca70cca2bfc57e811d3fe5d793d36ae127c98
Felipe Vilarinho <fvilarinho@gmail.com>
2022-08-11 11:39:59 -0300

    Code improvements in logging and data storage.

--- kafka/broker/bin/startup.sh
+++ kafka/broker/bin/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Setup the startup.
 $BIN_DIR/setup.sh
 
-/opt/kafka/bin/kafka-server-start.sh $ETC_DIR/server.properties
+# Start kafka specifying the configuration file.
+/opt/kafka/bin/kafka-server-start.sh $ETC_DIR/server.properties > $LOGS_DIR/kafka.log &
+
+tail -f $LOGS_DIR/kafka.log
