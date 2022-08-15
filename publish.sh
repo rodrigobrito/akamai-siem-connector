d5c46a07b07bcfd0c19a5f589e84e80817f18d13
Felipe Vilarinho <fvilarinho@gmail.com>
2022-08-15 09:18:19 -0300

    Improvements in publish the docker images. Define the standard for deploy.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Publish the container images in the docker registry repository.
+source ./.env
+
+echo $REPOSITORY_PASSWORD | docker login -u $REPOSITORY_ID $REPOSITORY_URL --password-stdin
+
 docker-compose --profile build push
