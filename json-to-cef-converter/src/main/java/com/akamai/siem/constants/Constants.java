fec5fe97ddbdd169d09c8e18fbceb46268c551fe
fvilarinho <fvilarinho@gmail.com>
2022-09-09 18:58:12 -0300

    Update json to cef converter.

--- json-to-cef-converter/src/main/java/com/akamai/siem/constants/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.akamai.siem.constants;
-
-public class Constants {
-    public static final String DEFAULT_APP_NAME = "json-to-cef-converter";
-}
--- json-to-cef-converter/src/main/java/com/akamai/siem/constants/SettingsConstants.java
+++
@@ -1,9 +0,0 @@
-package com.akamai.siem.constants;
-
-public class SettingsConstants {
-    public static final String DEFAULT_SETTINGS_FILENAME = "settings.json";
-    public static final String DEFAULT_HOME_DIR = "./";
-    public static final String DEFAULT_ETC_DIR = DEFAULT_HOME_DIR.concat("etc/");
-    public static final String DEFAULT_SETTINGS_FILEPATH = DEFAULT_ETC_DIR.concat(DEFAULT_SETTINGS_FILENAME);
-    public static final String SETTINGS_FILEPATH = "${ETC_DIR}".concat(DEFAULT_SETTINGS_FILENAME);
-}
