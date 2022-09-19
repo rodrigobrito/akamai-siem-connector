8a2a12e35f175c130f2f894c1ca8ec95c3ef68db
fvilarinho <fvilarinho@gmail.com>
2022-09-19 17:30:13 -0300

    Bug fixes in ingress configurations.

--- ingress/bin/setup.sh
+++ ingress/bin/setup.sh
@@ -4,12 +4,12 @@
 echo "export CLICOLOR=1" > ~/.bashrc
 echo "export PS1='[\u@\h:\W]$ '" >> ~/.bashrc
 
-for file in /etc/nginx/http.d/; do
+for file in $ETC_DIR/nginx/http.d/*.conf; do
   basename="/tmp/$(basename -- $file)"
 
   cp "$file" $basename
   sed -i -e 's|${QUEUE_DOMAIN}|'"$QUEUE_DOMAIN"'|g' $basename
   sed -i -e 's|${DASHBOARDS_DOMAIN}|'"$DASHBOARDS_DOMAIN"'|g' $basename
-  cp -f $basename $file
+  cp -f $basename "/etc/nginx/http.d/$(basename -- $file)"
   rm $basename
 done
