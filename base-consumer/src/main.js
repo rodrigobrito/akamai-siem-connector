f68312bb4baf4ca188c6f5e0bd19df11b960033a
Felipe Vilarinho <fvilarinho@gmail.com>
2022-09-21 12:34:06 -0300

    Bug fixes.

--- base-consumer/src/main.js
+++ base-consumer/src/main.js
@@ -4,38 +4,40 @@ const mqtt = require("mqtt");
 const os = require("os");
 
 const settingsObject = settings.loadSettings();
-const client = mqtt.connect("mqtt://" + settingsObject.scheduler, {clientId: os.hostname()});
+const mqttClient = mqtt.connect("mqtt://" + settingsObject.scheduler, {clientId: os.hostname()});
 
-client.subscribe(settingsObject.inputQueue);
+mqttClient.subscribe(settingsObject.inputQueue);
 
-client.on("connect",function(packet){
+mqttClient.on("connect",function(packet){
     let now = new Date();
 
     console.log("[" + now + "][" + os.hostname() + " connected to queue " + settingsObject.inputQueue + "]");
 });
 
-client.on("message", async function(queue, messageRaw, packet){
-    let now = new Date();
+mqttClient.on("message", async function(queue, messageRaw, packet){
+    try{
+        let now = new Date();
+        const messageObject = JSON.parse(messageRaw.toString());
 
-    const messageObject = JSON.parse(messageRaw.toString());
+        console.log("[" + now + "][" + os.hostname() + " received the job " + messageObject.job + " from queue " + settingsObject.inputQueue + "]");
 
-    console.log("[" + now + "][" + os.hostname() + " received the job " + messageObject.job + " from queue " + settingsObject.inputQueue + "]");
+        const result = akamaiSiem.fetchEvents(messageObject, settingsObject);
 
-    const promise = akamaiSiem.fetchEvents(messageObject, settingsObject);
+        result.then((eventsRaw) => {
+            mqttClient.publish(settingsObject.outputQueue, eventsRaw);
 
-    promise.then((eventsRaw) => {
-        now = new Date();
+            now = new Date();
 
-        client.publish(settingsObject.outputQueue, eventsRaw);
+            console.log("[" + now + "][" + os.hostname() + " published events of job " + messageObject.job + " to queue " + settingsObject.outputQueue + "]");
+        });
 
-        console.log("[" + now + "][" + os.hostname() + " published events of job " + messageObject.job + " to queue " + settingsObject.outputQueue + "]");
-    });
-
-    promise.catch((error, response) => {
+        result.catch((error) => console.log(error));
+    }
+    catch(error){
         console.log(error);
-    });
+    }
 });
 
-client.on("error", function(error){
+mqttClient.on("error", function(error){
     console.log(error);
 });
--- consumer/src/akamai-siem.js
+++ consumer/src/akamai-siem.js
@@ -1,57 +1,63 @@
 const EdgeGrid = require('akamai-edgegrid');
 const linebreak = "\\n";
 
-const fetchEvents = function (messageObject, settingsObject) {
-    const authParams = {
-        path: settingsObject.edgercFilename,
-        section: settingsObject.edgercSection
-    }
-
-    const eg = new EdgeGrid(authParams);
-    const fetchEventsParams = {
-        path: "/siem/v1/configs/" + settingsObject.configsIds,
-        method: "GET",
-        headers: {
-            Accept: "application/json"
-        },
-        qs: {
-            limit: messageObject.eventsPerJob,
-            from: messageObject.from,
-            to: messageObject.to
-        }
-    };
-
-    eg.auth(fetchEventsParams);
-
+const fetchEvents = async function (messageObject, settingsObject) {
     return new Promise(function (resolve, reject) {
-        eg.send(function (error, response, body) {
-            if (error) {
-                console.log(error, response, body);
-
-                return reject(error, response);
+        try{
+            const authParams = {
+                path: settingsObject.edgercFilename,
+                section: settingsObject.edgercSection
             }
 
-            let eventsBuffer = body.split(linebreak);
-            let eventsList = [];
-
-            eventsBuffer.forEach((item, index) => {
-                if (item.length > 0 && index < (eventsBuffer.length - 1)) {
-                    let eventObject = {
-                        key: messageObject.job + "-" + index,
-                        value: item.replace("'", "").replace(/\\/g, "")
-                    };
-
-                    eventsList.push(eventObject);
+            const eg = new EdgeGrid(authParams);
+            const fetchEventsParams = {
+                path: "/siem/v1/configs/" + settingsObject.configsIds,
+                method: "GET",
+                headers: {
+                    Accept: "application/json"
+                },
+                qs: {
+                    limit: messageObject.eventsPerJob,
+                    from: messageObject.from,
+                    to: messageObject.to
                 }
-            });
-
-            let eventsObject = {
-                "job": messageObject.job,
-                "events": eventsList
             };
 
-            return resolve(JSON.stringify(eventsObject));
-        });
+            eg.auth(fetchEventsParams);
+            eg.send(function (error, response, body) {
+                try{
+                    if (error)
+                        return reject(error);
+
+                    let eventsBuffer = body.split(linebreak);
+                    let eventsList = [];
+
+                    eventsBuffer.forEach((item, index) => {
+                        if (item.length > 0 && index < (eventsBuffer.length - 1)) {
+                            let eventObject = {
+                                key: messageObject.job + "-" + index,
+                                value: item.replace("'", "").replace(/\\/g, "")
+                            };
+
+                            eventsList.push(eventObject);
+                        }
+                    });
+
+                    let eventsObject = {
+                        "job": messageObject.job,
+                        "events": eventsList
+                    };
+
+                    return resolve(JSON.stringify(eventsObject));
+                }
+                catch(error){
+                    return reject(error);
+                }
+            });
+        }
+        catch(error){
+            return reject(error);
+        }
     });
 };
 
